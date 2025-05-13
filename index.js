dadbe6acdcad9d6db9c5251f2dbcba544d03ef71
wisdomfzolutions <wisdom@fzolutions.com>
2025-05-13 16:47:22 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Dwellify Backend API is running');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
