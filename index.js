6aa9bca2e84735f9c8d580ddc14a15331810b8b0
wisdomfzolutions <wisdom@fzolutions.com>
2025-05-13 17:23:26 +0100

    enviromental variable commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Middleware
 app.use(cors());
