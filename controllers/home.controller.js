ca76cca3aad7879d29d600a338a7c6d3c7f3691b
wisdomfzolutions <wisdom@fzolutions.com>
2025-05-14 08:34:45 +0100

    file structure commit

---
+++ controllers/home.controller.js
@@ -0,0 +1,3 @@
+export const getHome = (req, res) => {
+  res.send('🏡 Dwellify Backend API is running waiting to be consumed');
+};
--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-// index.js
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import homeRoutes from './routes/home.routes.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+if (!PORT) {
+  throw new Error('PORT is not defined in .env file');
+}
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Dwellify Backend API is running');
-});
+app.use('/', homeRoutes);
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
